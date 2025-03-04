0957e96b8a9adb8b3ee0fd1bae78b83786a4aeb4
Hasinur <hasinur.rahman1332@gmail.com>
2025-03-04 09:31:12 +0600

    displaying limited project in the homepage

--- src/components/RecentWork/RecentWork.jsx
+++ src/components/RecentWork/RecentWork.jsx
@@ -36,12 +36,12 @@ const RecentWork = () => {
 
         {/* Projects Grid Container */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-20">
-          {projects.map((project, idx) => (
+          {projects.slice(0, 3).map((project, idx) => (
             <motion.div
               variants={fadeUp}
               initial="hidden"
               whileInView="visible"
-              custom={idx * 0.3}
+              custom={idx * 0.2}
               key={project.id}
             >
               <ProjectCard project={project} />
