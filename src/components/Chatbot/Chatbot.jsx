2b38972fbd24edbee37d4892787a06db68b0d7da
Hasinur Rahman <hasinur.rahman1332@gmail.com>
2026-03-20 22:16:21 +0600

    updated ai model

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -24,7 +24,7 @@ const Chatbot = () => {
   const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-flash",
+    model: "gemini-2.5-lite",
   });
 
   const buildPrompt = (question) => {
